b9a3c438181af1f7797260a2132f639a6df9d13e
creditcoder <creditcoder@hotmail.com>
2020-03-30 23:36:19 +0500

    add activities page

--- src/pages/Activities/Activities.js
+++ src/pages/Activities/Activities.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { render } from "react-dom";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 import {
   Box,
   FormControl,
@@ -26,180 +31,13 @@ import PageSearchTitle from "../../components/PageTitle/PageSearchTitle";
 import Widget from "../../components/Widget/Widget";
 import { Typography } from "../../components/Wrappers/Wrappers";
 
-import { Calendar, Views, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.scss";
 
+const localizer = momentLocalizer(moment);
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 
-const localizer = momentLocalizer(moment);
-
-const events = [
-  {
-    id: 0,
-    title: "All Day Event very long title",
-    allDay: true,
-    start: new Date(2015, 3, 0),
-    end: new Date(2015, 3, 1),
-  },
-  {
-    id: 1,
-    title: "Long Event",
-    start: new Date(2015, 3, 7),
-    end: new Date(2015, 3, 10),
-  },
-
-  {
-    id: 2,
-    title: "DTS STARTS",
-    start: new Date(2016, 2, 13, 0, 0, 0),
-    end: new Date(2016, 2, 20, 0, 0, 0),
-  },
-
-  {
-    id: 3,
-    title: "DTS ENDS",
-    start: new Date(2016, 10, 6, 0, 0, 0),
-    end: new Date(2016, 10, 13, 0, 0, 0),
-  },
-
-  {
-    id: 4,
-    title: "Some Event",
-    start: new Date(2015, 3, 9, 0, 0, 0),
-    end: new Date(2015, 3, 10, 0, 0, 0),
-  },
-  {
-    id: 5,
-    title: "Conference",
-    start: new Date(2015, 3, 11),
-    end: new Date(2015, 3, 13),
-    desc: "Big conference for important people",
-  },
-  {
-    id: 6,
-    title: "Meeting",
-    start: new Date(2015, 3, 12, 10, 30, 0, 0),
-    end: new Date(2015, 3, 12, 12, 30, 0, 0),
-    desc: "Pre-meeting meeting, to prepare for the meeting",
-  },
-  {
-    id: 7,
-    title: "Lunch",
-    start: new Date(2015, 3, 12, 12, 0, 0, 0),
-    end: new Date(2015, 3, 12, 13, 0, 0, 0),
-    desc: "Power lunch",
-  },
-  {
-    id: 8,
-    title: "Meeting",
-    start: new Date(2015, 3, 12, 14, 0, 0, 0),
-    end: new Date(2015, 3, 12, 15, 0, 0, 0),
-  },
-  {
-    id: 9,
-    title: "Happy Hour",
-    start: new Date(2015, 3, 12, 17, 0, 0, 0),
-    end: new Date(2015, 3, 12, 17, 30, 0, 0),
-    desc: "Most important meal of the day",
-  },
-  {
-    id: 10,
-    title: "Dinner",
-    start: new Date(2015, 3, 12, 20, 0, 0, 0),
-    end: new Date(2015, 3, 12, 21, 0, 0, 0),
-  },
-  {
-    id: 11,
-    title: "Birthday Party",
-    start: new Date(2015, 3, 13, 7, 0, 0),
-    end: new Date(2015, 3, 13, 10, 30, 0),
-  },
-  {
-    id: 12,
-    title: "Late Night Event",
-    start: new Date(2015, 3, 17, 19, 30, 0),
-    end: new Date(2015, 3, 18, 2, 0, 0),
-  },
-  {
-    id: 12.5,
-    title: "Late Same Night Event",
-    start: new Date(2015, 3, 17, 19, 30, 0),
-    end: new Date(2015, 3, 17, 23, 30, 0),
-  },
-  {
-    id: 13,
-    title: "Multi-day Event",
-    start: new Date(2015, 3, 20, 19, 30, 0),
-    end: new Date(2015, 3, 22, 2, 0, 0),
-  },
-  {
-    id: 14,
-    title: "Today",
-    start: new Date(new Date().setHours(new Date().getHours() - 3)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3)),
-  },
-  {
-    id: 15,
-    title: "Point in Time Event",
-    start: new Date(2015, 3, 14, 15, 30, 0),
-    end: new Date(2015, 3, 14, 19, 0, 0),
-  },
-  {
-    id: 16,
-    title: "Video Record",
-    start: new Date(2015, 3, 14, 15, 30, 0),
-    end: new Date(2015, 3, 14, 19, 0, 0),
-  },
-  {
-    id: 17,
-    title: "Dutch Song Producing",
-    start: new Date(2015, 3, 14, 16, 30, 0),
-    end: new Date(2015, 3, 14, 20, 0, 0),
-  },
-  {
-    id: 18,
-    title: "Itaewon Halloween Meeting",
-    start: new Date(2015, 3, 14, 16, 30, 0),
-    end: new Date(2015, 3, 14, 17, 30, 0),
-  },
-  {
-    id: 19,
-    title: "Online Coding Test",
-    start: new Date(2015, 3, 14, 17, 30, 0),
-    end: new Date(2015, 3, 14, 20, 30, 0),
-  },
-  {
-    id: 20,
-    title: "An overlapped Event",
-    start: new Date(2015, 3, 14, 17, 0, 0),
-    end: new Date(2015, 3, 14, 18, 30, 0),
-  },
-  {
-    id: 21,
-    title: "Phone Interview",
-    start: new Date(2015, 3, 14, 17, 0, 0),
-    end: new Date(2015, 3, 14, 18, 30, 0),
-  },
-  {
-    id: 22,
-    title: "Cooking Class",
-    start: new Date(2015, 3, 14, 17, 30, 0),
-    end: new Date(2015, 3, 14, 19, 0, 0),
-  },
-  {
-    id: 23,
-    title: "Go to the gym",
-    start: new Date(2015, 3, 14, 18, 30, 0),
-    end: new Date(2015, 3, 14, 20, 0, 0),
-  },
-];
-
-let allViews = Object.keys(Views).map(k => Views[k]);
-
-export default function Activities() {
+function Activities(props) {
   var classes = useStyles();
 
   const [date, setDate] = useState(new Date("2014-08-18T21:11:54"));
@@ -214,13 +52,126 @@ export default function Activities() {
     setDate(d);
   };
 
+  const now = new Date();
+  const events = [
+    {
+      id: 0,
+      title: "All Day Event very long title",
+      allDay: true,
+      start: new Date(2015, 3, 0),
+      end: new Date(2015, 3, 1),
+    },
+    {
+      id: 1,
+      title: "Long Event",
+      start: new Date(2015, 3, 7),
+      end: new Date(2015, 3, 10),
+    },
+
+    {
+      id: 2,
+      title: "DTS STARTS",
+      start: new Date(2016, 2, 13, 0, 0, 0),
+      end: new Date(2016, 2, 20, 0, 0, 0),
+    },
+
+    {
+      id: 3,
+      title: "DTS ENDS",
+      start: new Date(2016, 10, 6, 0, 0, 0),
+      end: new Date(2016, 10, 13, 0, 0, 0),
+    },
+
+    {
+      id: 4,
+      title: "Some Event",
+      start: new Date(2015, 3, 9, 0, 0, 0),
+      end: new Date(2015, 3, 10, 0, 0, 0),
+    },
+    {
+      id: 5,
+      title: "Conference",
+      start: new Date(2015, 3, 11),
+      end: new Date(2015, 3, 13),
+      desc: "Big conference for important people",
+    },
+    {
+      id: 6,
+      title: "Meeting",
+      start: new Date(2015, 3, 12, 10, 30, 0, 0),
+      end: new Date(2015, 3, 12, 12, 30, 0, 0),
+      desc: "Pre-meeting meeting, to prepare for the meeting",
+    },
+    {
+      id: 7,
+      title: "Lunch",
+      start: new Date(2015, 3, 12, 12, 0, 0, 0),
+      end: new Date(2015, 3, 12, 13, 0, 0, 0),
+      desc: "Power lunch",
+    },
+    {
+      id: 8,
+      title: "Meeting",
+      start: new Date(2015, 3, 12, 14, 0, 0, 0),
+      end: new Date(2015, 3, 12, 15, 0, 0, 0),
+    },
+    {
+      id: 9,
+      title: "Happy Hour",
+      start: new Date(2015, 3, 12, 17, 0, 0, 0),
+      end: new Date(2015, 3, 12, 17, 30, 0, 0),
+      desc: "Most important meal of the day",
+    },
+    {
+      id: 10,
+      title: "Dinner",
+      start: new Date(2015, 3, 12, 20, 0, 0, 0),
+      end: new Date(2015, 3, 12, 21, 0, 0, 0),
+    },
+    {
+      id: 11,
+      title: "Birthday Party",
+      start: new Date(2015, 3, 13, 7, 0, 0),
+      end: new Date(2015, 3, 13, 10, 30, 0),
+    },
+    {
+      id: 12,
+      title: "Late Night Event",
+      start: new Date(2015, 3, 17, 19, 30, 0),
+      end: new Date(2015, 3, 18, 2, 0, 0),
+    },
+    {
+      id: 12.5,
+      title: "Late Same Night Event",
+      start: new Date(2015, 3, 17, 19, 30, 0),
+      end: new Date(2015, 3, 17, 23, 30, 0),
+    },
+    {
+      id: 13,
+      title: "Multi-day Event",
+      start: new Date(2015, 3, 20, 19, 30, 0),
+      end: new Date(2015, 3, 22, 2, 0, 0),
+    },
+    {
+      id: 14,
+      title: "Today",
+      start: new Date(new Date().setHours(new Date().getHours() - 3)),
+      end: new Date(new Date().setHours(new Date().getHours() + 3)),
+    },
+    {
+      id: 15,
+      title: "Point in Time Event",
+      start: now,
+      end: now,
+    },
+  ];
+
   return (
     <>
       <PageSearchTitle title="Actividades" />
-
       <Box
         display="flex"
-        flextDirection="row"
+        flexDirection="row"
         justifyContent="space-between"
         alignItems="center"
       >
@@ -259,26 +210,17 @@ export default function Activities() {
         </Box>
         <Box flex={4} />
       </Box>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
-
-      <Calendar
-        events={events}
-        views={allViews}
-        step={60}
-        showMultiDayTimes
-        // max={dates.add(dates.endOf(new Date(2015, 17, 1), "day"), -1, "hours")}
-        defaultDate={new Date(2015, 3, 1)}
-        // components={{
-        //   timeSlotWrapper: ColoredDateCellWrapper,
-        // }}
-        localizer={localizer}
-      />
+      <div style={{ height: "500pt" }}>
+        <Calendar
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          defaultDate={moment().toDate()}
+          localizer={localizer}
+        />
+      </div>
     </>
   );
 }
+
+export default Activities;
